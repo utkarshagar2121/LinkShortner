5516ba7a99ac5c5ceca14178eaa8098ab59e68ef
Utkarsh Agarwal <127212603+utkarshagar2121@users.noreply.github.com>
2025-02-26 00:02:13 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
-import AppLayout from "./Layout/ApplAyout";
+import AppLayout from "./Layout//AppLayout";
 import LandingPage from "./Pages/LandingPage";
 import Dashboard from "./Pages/Dashboard";
 import Auth from "./Pages/Auth";
 import Link from "./Pages/Link";
 import Redirectlink from "./Pages/Redirectlink";
 import UrlProvider from "./Context";
-import requireAuth from "./components/requireAuth";
+
 import RequireAuth from "./components/requireAuth";
 function App() {
   const router = createBrowserRouter([
